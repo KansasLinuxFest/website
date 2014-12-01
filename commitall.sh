cbe2f644536bc06bda04f7cf7eca36e8a0cc5b60
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-11-30 20:50:10 -0600

    now events are working

--- commitall.sh
+++ commitall.sh
@@ -2,4 +2,6 @@ for x in `cat repos.txt`; do echo $x;
     pushd $x; 
     git status; 
     git commit -m 'wip' -a; 
-    git clean -f; popd; done 
+    git clean -f; 
+    git push;
+    popd; done 
